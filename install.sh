9fbc769c91b244b801fe7b97e6bf13ee3d758b65
LinuxPengueni1901 <tahakeskinofficial1901@gmail>
2025-12-28 16:35:18 +0300

    Update install.sh and add metainfo xml

--- install.sh
+++ install.sh
@@ -5,13 +5,19 @@ mkdir -p /app/share/whatpenguin
 mkdir -p /app/share/applications
 mkdir -p /app/share/icons/hicolor/128x128/apps
 
+mkdir -p /app/share/metainfo
+
 install -m 755 main.py /app/share/whatpenguin/
 install -m 644 window.py /app/share/whatpenguin/
 install -m 644 whatpenguin.png /app/share/whatpenguin/
-install -m 644 whatpenguin.png /app/share/icons/hicolor/128x128/apps/whatpenguin.png
+# İkonu AppID ismiyle kopyala (Flathub gereksinimi)
+install -m 644 whatpenguin.png /app/share/icons/hicolor/128x128/apps/io.github.linuxpengueni1901.whatpenguin.png
 
 # Desktop file
-install -m 644 whatpenguin.desktop /app/share/applications/
+install -m 644 io.github.linuxpengueni1901.whatpenguin.desktop /app/share/applications/
+
+# Metainfo
+install -m 644 io.github.linuxpengueni1901.whatpenguin.metainfo.xml /app/share/metainfo/
 
 # Wrapper script
 echo "#!/bin/bash" > /app/bin/whatpenguin
