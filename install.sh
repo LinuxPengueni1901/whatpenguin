2607b15e517fc12bc0b79374291f22d8997736ca
LinuxPengueni1901 <tahakeskinofficial1901@gmail>
2025-12-28 16:41:16 +0300

    Resize icon to 512x512 and update install path

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir -p /app/bin
 mkdir -p /app/share/whatpenguin
 mkdir -p /app/share/applications
-mkdir -p /app/share/icons/hicolor/128x128/apps
+mkdir -p /app/share/icons/hicolor/512x512/apps
 
 mkdir -p /app/share/metainfo
 
@@ -11,7 +11,7 @@ install -m 755 main.py /app/share/whatpenguin/
 install -m 644 window.py /app/share/whatpenguin/
 install -m 644 whatpenguin.png /app/share/whatpenguin/
 # İkonu AppID ismiyle kopyala (Flathub gereksinimi)
-install -m 644 whatpenguin.png /app/share/icons/hicolor/128x128/apps/io.github.linuxpengueni1901.whatpenguin.png
+install -m 644 whatpenguin.png /app/share/icons/hicolor/512x512/apps/io.github.linuxpengueni1901.whatpenguin.png
 
 # Desktop file
 install -m 644 io.github.linuxpengueni1901.whatpenguin.desktop /app/share/applications/
